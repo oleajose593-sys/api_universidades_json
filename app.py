bb2aa56bdcbd34a02b14b3f58b24e07cd5fbd134
oleajose593-sys <oleajose593@gmail.com>
2026-04-15 21:19:07 -0600

    agregando endpoint POST universidades

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import json
 
 app = Flask(__name__)
 
+# 🔹 VER JSON
 @app.route("/")
 def ver_json():
 
     with open("universidades.json", encoding="utf-8") as f:
-
         data = json.load(f)
 
     return Response(
-
         json.dumps(data, indent=4, ensure_ascii=False),
-
         mimetype="application/json"
     )
 
-if __name__ == "__main__":
 
+# 🔥 RECIBIR JSON DESDE TU ADMIN
+@app.route("/universidades", methods=["POST"])
+def actualizar_json():
+
+    nuevas_universidades = request.json
+
+    with open("universidades.json", "w", encoding="utf-8") as f:
+        json.dump(nuevas_universidades, f, indent=4, ensure_ascii=False)
+
+    return {"mensaje": "JSON actualizado correctamente"}, 200
+
+
+if __name__ == "__main__":
     app.run(port=5001)
